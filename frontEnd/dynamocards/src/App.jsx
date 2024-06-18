a0aa167b497047a605757822eb732230c00adf3c
Kriti A <kritiajwani6148@gmail.com>
2024-06-18 09:05:23 -0400

    Made a new repo to point to main

--- frontEnd/dynamocards/src/App.jsx
+++ frontEnd/dynamocards/src/App.jsx
@@ -1,41 +1,43 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function App(){
+function App() {
   const [youtubeLink, setYoutubeLink] = useState("");
   const [responseData, setResponseData] = useState(null);
 
-
-  const handlelinkchange = (event) => {
+  const handleLinkChange = (event) => {
     setYoutubeLink(event.target.value);
   };
 
   const sendLink = async () => {
     try {
-      const response = await axios.post("http://localhost:8000/analyze_video",{
-        youtube_link : youtubeLink,
+      const response = await axios.post("http://localhost:8000/analyze_video", {
+        youtube_link: youtubeLink,
       });
-      setResponseData(response.data);cd 
-    } catch (error){
+      setResponseData(response.data);
+    } catch (error) {
       console.log(error);
     }
-  }; return (
-    <div className='App'>
-    <h1>Youtube Link to Falsh Cards Generator</h1>
-    <input type='text'
-    placeholder='Paste Link here'
-    value={youtubeLink}
-    onChange={handleLinkchange}
-    />
-    <button onClick={sendLink}></button>
-    { responseData &&(
-      <div>
-        <h2>Response Data:</h2>
-        <p> {JSON.stringify(responseData, null, 2)}</p>
-      </div> 
-      )
-    }
-  </div>
-  )
+  };
+
+  return (
+    <div className="App">
+      <h1>Youtube Link to Flash Cards Generator</h1>
+      <input 
+        type="text"
+        placeholder="Paste Link here"
+        value={youtubeLink}
+        onChange={handleLinkChange}
+      />
+      <button onClick={sendLink}>Generate Flash Cards</button>
+      {responseData && (
+        <div>
+          <h2>Response Data:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
 }
+
 export default App;
